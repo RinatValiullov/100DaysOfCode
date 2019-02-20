c9abc2e6c085e3e3511ff5d5f015779169fae030
RinatValiullov <4rontender@gmail.com>
2019-02-20 23:18:28 +0500

    added initial sass_compile task in gulp

--- gulp/compile-sass/gulpfile.js
+++ gulp/compile-sass/gulpfile.js
@@ -1,3 +1,12 @@
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync').create();
+
+// compile scss into css
+function sass_compile() {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+}
+
+exports.sass_compile = sass_compile
