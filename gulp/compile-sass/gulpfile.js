48b45553b47d82ef2d61091dad948fa1be6322ca
RinatValiullov <4rontender@gmail.com>
2019-02-21 00:35:46 +0500

    finish gulpfile.js
    added watch task

--- gulp/compile-sass/gulpfile.js
+++ gulp/compile-sass/gulpfile.js
@@ -5,8 +5,21 @@ let gulp = require('gulp'),
 // compile scss into css
 function sass_compile() {
     return gulp.src('./sass/**/*.scss')
-        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream())
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./sass/**/*.scss', sass_compile)
+    gulp.watch('./*.html').on('change', browserSync.reload)
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload)
 }
 
 exports.sass_compile = sass_compile
+exports.watch = watch;
