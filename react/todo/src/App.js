1e1d981aad039ce774e51a6f0a1c0fa79e774146
RinatValiullov <4rontender@gmail.com>
2019-02-24 23:41:42 +0500

    added Todos component

--- react/todo/src/App.js
+++ react/todo/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
   render() {
     return ( 
       <div className="App">
-        <h1>App Component</h1>
+        <Todos />
       </div>
     );
   }
---
+++ react/todo/src/components/Todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+    render() {
+        return (
+            <>
+                <h1>Todos</h1>
+            </>
+        );
+    }
+}
+
+export default Todos;
