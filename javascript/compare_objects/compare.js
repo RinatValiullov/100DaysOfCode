c9253cd321b488e1e26ed71af680234f0334226d
RinatValiullov <4rontender@gmail.com>
2019-02-27 12:25:23 +0500

    finish compare_objects project

--- javascript/compare_objects/compare.js
+++ javascript/compare_objects/compare.js
@@ -1,12 +1,3 @@
-/**
- *  Compare two items and push non-matches to object
- *  @param {*}      item1 The first item
- *  @param {*}      item2 The second item
- *  @param {String} key   The key in object
- */
-
-let compare = (item1, item2, key) => {};
-
 let order1 = {
     sandwitch: "tuna",
     chips: true,
@@ -45,10 +36,105 @@ let diff = (obj1, obj2) => {
     let diffs = {},
         key;
 
-    // Compare objects
+    /* Methods */
+
+    /**
+     * Check if two arrays are equal
+     * @param {Array} arr1  The first array
+     * @param {Array} arr2  The second array
+     * @returns {Boolean}   If true, both arrays are equal
+     */
+
+    let arrayMatches = (arr1, arr2) => {
+        // Check if arrays are the same length
+        if (arr1.length !== arr2.length) {
+            return false;
+        }
+
+        // Check if all items exist and are in the same order
+        for (let index = 0; index < arr1.length; index++) {
+            if (arr1[index] !== arr2[index]) {
+                return false;
+            }
+        }
+
+        // Otherwise, return true
+        return true;
+    };
+
+    /**
+     *  Compare two items and push non-matches to object
+     *  @param {*}      item1 The first item
+     *  @param {*}      item2 The second item
+     *  @param {String} key   The key in object
+     */
+
+    let compare = (item1, item2, key) => {
+        // Get the object type
+        let type1 = Object.prototype.toString.call(item1);
+        let type2 = Object.prototype.toString.call(item2);
+
+        // If type2 is undefined it has been removed
+        if (type2 === "[object Undefined]") {
+            diffs[key] = null;
+            return;
+        }
+
+        // If items are different types
+        if (type1 !== type2) {
+            diffs[key] = item2;
+            return;
+        }
+
+        // If an object, compare recursively
+        if (type1 === "[object Object]") {
+            let objDiff = diff(item1, item2);
+            if (Object.keys(objDiff).length > 1) {
+                diffs[key] = objDiff;
+            }
+            return;
+        }
+
+        // If an array, compare
+        if (type1 === "[object Array]") {
+            if (!arrayMatches(item1, item2)) {
+                diffs[key] = item2;
+            }
+            return;
+        }
+
+        // Else if it's a function, convert to string and compare
+        // Otherwise, just compare
+        if (type1 === "[object Function]") {
+            if (item1.toString() !== item2.toString()) {
+                diffs[key] = item2;
+            }
+        } else {
+            if (item1 !== item2) {
+                diffs[key] = item2;
+            }
+        }
+    };
+
+    /* Compare objects */
+
+    // Loop through the first object
     for (key in obj1) {
         if (obj1.hasOwnProperty(key)) {
             compare(obj1[key], obj2[key], key);
         }
     }
+
+    // Loop through the second object
+    for (key in obj2) {
+        if (obj2.hasOwnProperty(key)) {
+            if (!obj1[key] && obj1[key] !== obj2[key]) {
+                diffs[key] = obj2[key];
+            }
+        }
+    }
+
+    return diffs;
 };
+
+console.log(diff(order1, order2));
