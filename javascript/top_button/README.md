9bde890f219526a4e92380177671a6b0aa3cdf1e
RinatValiullov <4rontender@gmail.com>
2019-03-10 01:19:39 +0500

    added top_button project
    understanding "scroll" event

---
+++ javascript/top_button/README.md
@@ -0,0 +1,1 @@
+## Creating top button and understand scroll event (DOM Level 2)
---
+++ javascript/top_button/main.js
@@ -0,0 +1,27 @@
+let show = document.querySelector("#showScroll");
+let btn = document.querySelector("#updown");
+
+let showBottom = show.getBoundingClientRect().bottom + window.pageYOffset;
+
+window.addEventListener("scroll", function (event) {
+    let scrolled = window.pageYOffset;
+    show.innerHTML = `${scrolled}px to back to top`;
+
+    if (show.classList.contains("fixed") && window.pageYOffset < showBottom) {
+        show.classList.remove("fixed");
+        btn.className = "";
+    } else if (window.pageYOffset > showBottom) {
+        show.classList.add("fixed");
+        btn.className = "up";
+    }
+});
+
+btn.addEventListener("click", event => {
+    let pageY = window.pageYOffset;
+    switch (btn.className) {
+        case "up":
+            window.scrollTo(0, 0);
+            btn.className = "";
+            break;
+    }
+});
