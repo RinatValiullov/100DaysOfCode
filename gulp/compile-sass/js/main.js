d72b87b0be5c3f35fa7d18572cca41815b4ef7d2
RinatValiullov <4rontender@gmail.com>
2019-07-08 22:30:11 +0500

    updated compile-sass project

--- gulp/compile-sass/js/main.js
+++ gulp/compile-sass/js/main.js
@@ -1,1 +1,15 @@
-console.log('I\'m watched too');
+let tasks = [
+    'gulp',
+    'task',
+    'finished'
+];
+
+let ul = document.createElement('ul');
+
+tasks.map(el => {
+    let li = document.createElement('li');
+    li.innerText = el;
+    ul.appendChild(li)
+});
+
+document.body.appendChild(ul);
