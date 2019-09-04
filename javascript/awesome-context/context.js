083191b058a61b7d211c31a7246d7ddc01e0c41b
RinatValiullov <4rontender@gmail.com>
2019-09-04 21:37:38 +0500

    passed arguments to bind method

--- javascript/awesome-context/context.js
+++ javascript/awesome-context/context.js
@@ -6,10 +6,12 @@ let user = {
     nick: '4rontender',
     age: 34,
     greet: hello,
-    showInfo() {
+    showInfo(job, phone) {
         console.group(`${this.nick} info:`)
         console.log(`Nickname is: ${this.nick}`);
         console.log(`Age is: ${this.age}`);
+        console.log(`Job is: ${job}`);
+        console.log(`Phone is: ${phone}`);
         console.groupEnd()
     }
 };
@@ -19,7 +21,7 @@ let newUser = {
     age: 555
 };
 
-user.showInfo.bind(newUser)();
+user.showInfo.bind(newUser, 'Builder', '8-565-656-00-01')();
 
 user.greet();
 user.showInfo();
