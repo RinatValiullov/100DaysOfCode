347039acba99d14ca5d3c78b516d591f6523a5f4
RinatValiullov <4rontender@gmail.com>
2019-09-07 12:36:29 +0500

    added condition for supporting matchAll method

--- javascript/whitespace_regex-js2019/ws-reg.js
+++ javascript/whitespace_regex-js2019/ws-reg.js
@@ -1,10 +1,18 @@
 let regularXP = /\p{White_Space}+/ug;
 let string = 'milk and sugar',
-    replacedString;
+  replacedString;
 
-for (const space of string.matchAll(regularXP)) {
-  console.info(space);
+if ('matchAll' in String.prototype) {
+  console.log('your node supports matchAll');
+  for (const space of string.matchAll(regularXP)) {
+    console.info(space);
+  }
+} else {
+  console.log(
+    `Sorry. Your node doesn\'t support matchAll. Update it, please, to 12.0.0 at least.`
+  );
 }
 
 replacedString = string.replace(regularXP, '*');
+console.log(`\n`);
 console.log(replacedString);
