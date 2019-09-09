95e4effbdfeca8031de899aeed258f7332d1dc6d
RinatValiullov <4rontender@gmail.com>
2019-09-09 13:30:54 +0500

    looping map using MapIterator

--- javascript/map-datastructure/loopMap.js
+++ javascript/map-datastructure/loopMap.js
@@ -8,3 +8,19 @@ people
   .set('Brons', 55)
   .set('Dazy', 64)
   .set('Merk', 99);
+
+
+// loop through map
+// 1. Using MapIterator
+
+let namesOfPeople = people.keys();
+let valuesOfPeople = people.values();
+
+console.group('people');
+console.log(namesOfPeople.next(), valuesOfPeople.next());
+console.log(namesOfPeople.next(), valuesOfPeople.next());
+console.log(namesOfPeople.next(), valuesOfPeople.next());
+console.log(namesOfPeople.next(), valuesOfPeople.next());
+console.log(namesOfPeople.next(), valuesOfPeople.next());
+console.log(namesOfPeople.next(), valuesOfPeople.next());
+console.groupEnd('people');
