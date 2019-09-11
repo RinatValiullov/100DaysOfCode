fb6a9129439787ddde069b2a434d1859f4318fa0
RinatValiullov <4rontender@gmail.com>
2019-09-11 19:47:35 +0500

    used prepend method

--- jQuery/add-node/add.js
+++ jQuery/add-node/add.js
@@ -1,5 +1,6 @@
 // Base elements
-let simpleDiv = $('<div>Simple div</div>');
+let simpleDiv = $('<div>Simple div (appended)</div>');
+let simpleDivPrep = $('<div>Simple div (prepended)</div>');
 
 
 $('.clear').on('click', function () {
@@ -13,4 +14,10 @@ $('.add').on('click', function () {
 
 $('.addto').on('click', function () {
     $(simpleDiv).appendTo($('.inside fieldset'));
+});
+
+
+// prepend method
+$('.prep').on('click', function () {
+    $('.inside fieldset').prepend(simpleDivPrep);
 });
