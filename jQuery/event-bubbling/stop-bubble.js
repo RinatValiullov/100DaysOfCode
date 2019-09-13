a7e55c73772ba44e7d650864693f0392594301d6
RinatValiullov <4rontender@gmail.com>
2019-09-13 09:47:48 +0500

    added example with stopPropagation method

--- jQuery/event-bubbling/stop-bubble.js
+++ jQuery/event-bubbling/stop-bubble.js
@@ -3,6 +3,7 @@ let elements = $('*');
 for (let element of elements) {
   (function(current) {
     $(current).on('click', function(event) {
+      event.stopPropagation();
       console.log(event.isPropagationStopped());
       console.log(
         `${current.tagName}.${current.className} --> ${event.target.tagName}.${event.target.className}`
