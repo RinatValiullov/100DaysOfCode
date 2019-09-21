b6875e5a92556c13f6d493294ae91703b0e13799
RinatValiullov <4rontender@gmail.com>
2019-09-21 23:06:57 +0500

    added base XMLHttpRequest object

--- javascript/send-request/xhr.js
+++ javascript/send-request/xhr.js
@@ -1,7 +1,15 @@
 let getBtn = document.querySelector('.get-btn');
 let sendBtn = document.querySelector('.send-btn');
 
-let getData = () => {};
+let getData = () => {
+    let url = 'https://reqres.in/api/users';
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url);
+
+    xhr.send(null);
+};
 
 let sendData = () => {};
 
