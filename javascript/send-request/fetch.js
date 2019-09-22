01e95177f4f1f9e57379d1b12bd71006eaa2d38d
RinatValiullov <4rontender@gmail.com>
2019-09-22 16:09:22 +0500

    sendData function (with fetch)

--- javascript/send-request/fetch.js
+++ javascript/send-request/fetch.js
@@ -1,15 +1,26 @@
 let getBtn = document.querySelector('.get-btn');
 let sendBtn = document.querySelector('.send-btn');
 
-let getData = () => {
-    fetch('https://reqres.in/api/users')
-        .then(response => response.json())
-        .then(responseData => console.log(responseData));
+let sendHttpRequest = (method, url, data) => {
+  return fetch(url, {
+    method,
+    body: JSON.stringify(data),
+    headers: data ? { 'Content-Type': 'application/json' } : {}
+  }).then(response => response.json());
 };
 
-let sendData = () => {};
+let getData = () => {
+  sendHttpRequest('GET', 'https://reqres.in/api/users').then(responseData =>
+    console.log(responseData)
+  );
+};
 
-
+let sendData = () => {
+  sendHttpRequest('POST', 'https://reqres.in/api/register', {
+    email: 'eve.holt@reqres.in',
+    password: 'pistol'
+  }).then(responseData => console.log(responseData));
+};
 
 getBtn.addEventListener('click', getData);
 sendBtn.addEventListener('click', sendData);
