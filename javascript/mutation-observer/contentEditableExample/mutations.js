2edcac5908a20e58a1a6f25ee914d846b41c3fa2
RinatValiullov <4rontender@gmail.com>
2019-09-27 13:27:54 +0500

    finished basics of contentEditableExample

--- javascript/mutation-observer/contentEditableExample/mutations.js
+++ javascript/mutation-observer/contentEditableExample/mutations.js
@@ -1,7 +1,12 @@
 let targetElement = document.querySelector('#target');
 
 let observer = new MutationObserver(mutationRecords => {
-  console.log(mutationRecords);
+  console.dir(mutationRecords);
 });
 
-observer.observe(targetElement, {});
+observer.observe(targetElement, {
+  childList: true,
+  subtree: true, // allow to track changes on a child 'i' element
+  characterDataOldValue: true,
+  attributeOldValue: true,
+});
