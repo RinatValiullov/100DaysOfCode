215001ca13af2e65e9f7707ecfc4df2163edc8d5
RinatValiullov <4rontender@gmail.com>
2019-10-02 22:20:17 +0500

    added eslint.json

--- vue/product-showcase/src/generate-uuid.js
+++ vue/product-showcase/src/generate-uuid.js
@@ -1,5 +1,6 @@
-let uuid = () => `id_${(Math.floor(Math.random() * 1e12)).toString(16)}`;
+let uuid = () => `id_${Math.floor(Math.random() * 1e12).toString(16)}`;
 
 let uniqueID = uuid();
 
+// eslint-disable-next-line no-console
 console.log(uniqueID);
