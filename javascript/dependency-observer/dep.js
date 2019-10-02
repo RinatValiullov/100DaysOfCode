e0de2ef830af1f8c09675b8d43b6840ce50758d4
RinatValiullov <4rontender@gmail.com>
2019-10-02 16:35:39 +0500

    added watcher function

--- javascript/dependency-observer/dep.js
+++ javascript/dependency-observer/dep.js
@@ -1,3 +1,4 @@
+// Observer pattern
 class Dep {
   constructor() {
     this.subscribers = [];
@@ -19,16 +20,32 @@ let dep = new Dep();
 let price = 15;
 let discount = 0.8;
 let total = 0;
-let target = () => {
+
+// let target = () => {
+//   total = price * discount;
+// };
+
+// dep.depend(); // add target to our subscribers
+
+// target(); // get the total
+
+// console.log('Get the total: ', total);
+// price = 25;
+// console.log('After changing the price, nothing changed: ', total);
+// dep.notify(); // run the subscribers
+// console.log('Right result: ', total);
+// price = 35;
+// dep.notify();
+// console.log('Right result: ', total);
+
+let target = 0;
+function watcher(myFunc) {
+  target = myFunc;
+  dep.depend();
+  target();
+  target = null;
+}
+
+watcher(() => {
   total = price * discount;
-};
-
-dep.depend(); // add target to our subscribers
-
-target(); // get the total
-
-console.log('Get the total: ', total);
-price = 25;
-console.log('After changing the price, nothing changed: ', total);
-dep.notify(); // run the subscribers
-console.log('Right result: ', total);
+});
