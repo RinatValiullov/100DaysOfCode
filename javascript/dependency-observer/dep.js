c884896ff04ef581b33538eaec30bae2080e9545
RinatValiullov <4rontender@gmail.com>
2019-10-03 08:30:36 +0500

    finish project

--- javascript/dependency-observer/dep.js
+++ javascript/dependency-observer/dep.js
@@ -1,3 +1,6 @@
+let data = { price: 5, discount: 0.8 };
+let target = null;
+
 // Observer pattern
 class Dep {
   constructor() {
@@ -15,68 +18,29 @@ class Dep {
   }
 }
 
-let dep = new Dep();
+Object.keys(data).forEach(key => {
+  let internalValue = data[key];
 
-let price = 15;
-let discount = 0.8;
-let total = 0;
+  let dep = new Dep();
 
-/* Solution: A Dependency Class */
-
-// let target = () => {
-//   total = price * discount;
-// };
-
-// dep.depend(); // add target to our subscribers
-
-// target(); // get the total
-
-// console.log('Get the total: ', total);
-// price = 25;
-// console.log('After changing the price, nothing changed: ', total);
-// dep.notify(); // run the subscribers
-// console.log('Right result: ', total);
-// price = 35;
-// dep.notify();
-// console.log('Right result: ', total);
-
-/* Solution: A Dependency Class */
-
-/* Solution: Watcher Function */
-
-// let target = 0;
-// function watcher(myFunc) {
-//   target = myFunc;
-//   dep.depend();
-//   target();
-//   target = null;
-// }
-
-// watcher(() => {
-//   total = price * discount;
-// });
-
-/* Solution: Watcher Function */
-
-/* Solution: Object.defineProperty() */
-
-let data = { price: 5, discount: 0.8 };
-
-let internalValue = data.price;
-
-Object.defineProperty(data, 'price', {
-  get() {
-    console.log(`Getting price: ${internalValue}`);
-    return internalValue;
-  },
-
-  set(value) {
-    console.log(`Setting price to: ${value}`);
-    internalValue = value;
-  }
+  Object.defineProperty(data, key, {
+    get() {
+      dep.depend(); // Remember the target we're running
+      return internalValue;
+    },
+    set(newValue) {
+      internalValue = newValue;
+      dep.notify(); // Re-run stored function
+    }
+  });
 });
 
-total = data.price * data.discount;
-data.price = 12;
+function watcher(myFunc) {
+  target = myFunc;
+  target();
+  target = null;
+}
 
-/* Solution: Object.defineProperty() */
+watcher(() => {
+  data.total = data.price * data.discount;
+});
