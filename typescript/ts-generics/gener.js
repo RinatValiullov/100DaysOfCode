4727bc8e834284a795b0e31f0454609a751a67f5
RinatValiullov <4rontender@gmail.com>
2019-10-11 13:50:15 +0500

    extended example ts-generics a little (added case with base restrictions)

--- typescript/ts-generics/gener.js
+++ typescript/ts-generics/gener.js
@@ -1,2 +1,10 @@
 var arrayOfNumbers = [1, 1, 2, 3, 5, 8, 13, 21];
-console.log(arrayOfNumbers);
+var arrayOfStrings = ['4ront', 'JavaScript'];
+function reverse(array) {
+    return array.reverse();
+}
+function getLength(arg) {
+    return arg.length;
+}
+var result1 = reverse(arrayOfNumbers);
+reverse(arrayOfStrings);
--- typescript/ts-generics/gener.ts
+++ typescript/ts-generics/gener.ts
@@ -1,9 +1,19 @@
-let arrayOfNumbers: Array<number> = [1,1,2,3,5,8,13,21];
+let arrayOfNumbers: Array<number> = [1, 1, 2, 3, 5, 8, 13, 21];
 let arrayOfStrings: Array<string> = ['4ront', 'JavaScript'];
 
 function reverse<T>(array: T[]): T[] {
-    return array.reverse();
+  return array.reverse();
 }
 
-reverse(arrayOfNumbers);
-reverse(arrayOfStrings);
+// Base restrictions of Generic Types
+
+interface Length {
+  length: number;
+}
+
+function getLength<T extends Length>(arg: T): number {
+  return arg.length;
+}
+
+let result1 = reverse(arrayOfNumbers);
+let result2 = reverse(arrayOfStrings);
