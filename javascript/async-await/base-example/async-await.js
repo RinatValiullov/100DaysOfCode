255ee2c6f7beb8b504b1cfbd416e0b4731320083
RinatValiullov <4rontender@gmail.com>
2019-10-13 15:30:09 +0500

    stringified output

--- javascript/async-await/base-example/async-await.js
+++ javascript/async-await/base-example/async-await.js
@@ -4,8 +4,8 @@ async function getChuckJoke(id) {
   let response = await fetch(`https://api.chucknorris.io/jokes/${id}`);
   let data = await response.json();
 
-  console.log(data.value);
-  // return data;
+  console.log(JSON.stringify(data, null, 2));
+  return data;
 }
 
 let result = getChuckJoke('nrxnz9iyRqqgNix3GzVajA');
