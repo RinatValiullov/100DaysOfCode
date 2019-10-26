b16174f2812cd7f1b5f232179262992071e9d424
RinatValiullov <4rontender@gmail.com>
2019-10-26 11:42:02 +0500

    added conditional rendering

--- react/react-sample/src/components/Heading/Heading.js
+++ react/react-sample/src/components/Heading/Heading.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import './Heading.css';
 
+let day = 'afternoon';
+
 function Heading(props) {
-  return <h1 className={props.title}>Hello, React!!!</h1>;
+  return (
+    <h1 className={props.title}>
+      {day === 'afternoon'
+        ? 'Good Afternoon,'
+        : day === 'afternoon'
+        ? 'Good Evening,'
+        : 'You must determine the time of day'}
+      <i> React!</i>
+    </h1>
+  );
 }
 
 export default Heading;
