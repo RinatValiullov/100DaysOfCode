3301ab5a42dac25a84d9efd1bb107ca27f4316e9
RinatValiullov <4rontender@gmail.com>
2019-10-31 00:23:19 +0500

    added Layout component-wrapper

--- react/use-bootstrap/src/App.js
+++ react/use-bootstrap/src/App.js
@@ -4,18 +4,21 @@ import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
 import { NoMatch } from './pages/NoMatch';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
     </Fragment>
   );
 }
---
+++ react/use-bootstrap/src/components/Layout.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export let Layout = props => <Container>{props.children}</Container>;
