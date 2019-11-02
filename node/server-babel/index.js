15fbd544ea05b99252a692bf5318dbc5ad1842fa
RinatValiullov <4rontender@gmail.com>
2019-11-02 11:22:20 +0500

    added nodemon for watching files changing

--- node/server-babel/index.js
+++ node/server-babel/index.js
@@ -3,8 +3,8 @@ import http from 'http';
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, Babel and Node!\n');
-}).listen(4327, 'localhost');
+}).listen(4327, '127.0.0.1');
 
-console.log(`Server running at http://localhost:4327`)
+console.log(`Server running at http://127.0.0.1:4327/`)
 
 export default server;
