6e5d8e9de181a93f84d73dfe1c3c5415149ab631
RinatValiullov <4rontender@gmail.com>
2019-12-06 10:21:08 +0500

    added jsonplaceholder API for getting user ID (random number)

--- javascript/learn-promises/promise-all/promises.js
+++ javascript/learn-promises/promise-all/promises.js
@@ -1,16 +1,36 @@
-let x = 5;
-let y = 7;
+const fetch = require('node-fetch');
+const URL = 'https://jsonplaceholder.typicode.com/users';
 
-let fetchX = () => Promise.resolve(x);
-let fetchY = () => Promise.resolve(y);
+// helper function for getting random number from determined range
+// const getRandomNumber = ({ min = 1, max = 10 } = {}) =>
+//   Math.floor(Math.random() * (max - min)) + min;
+
+// get max parameter for getRandomNumber function
+// const maxLength = fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(d => d.json())
+//   .then(d => d.length);
+
+const randomUserX = getRandomNumber({ max: 10 });
+const randomUserY = getRandomNumber({ max: 10 });
+
+const fetchX = url =>
+  fetch(`${url}/${randomUserX}`)
+    .then(userData => userData.json())
+    .then(userID => userID['id']);
+
+const fetchY = url =>
+  fetch(`${url}/${randomUserY}`)
+    .then(userData => userData.json())
+    .then(userID => userID['id']);
 
 function add(xPromise, yPromise) {
-  return Promise.all([xPromise, yPromise]).then(
-    values => values[0] + values[1]
-  );
+  return Promise.all([xPromise, yPromise]).then(values => {
+    console.log(values); // an array of values from the previously resolved promises
+    return values[0] + values[1];
+  });
 }
 
-let sum = add(fetchX(), fetchY());
+const sum = add(fetchX(URL), fetchY(URL));
 
 sum.then(
   getSum => console.log(getSum),
