79d7f3034f7b218b37706df59481a7fe6c84604f
RinatValiullov <4rontender@gmail.com>
2019-12-09 22:17:08 +0500

    added another lookahead example

--- javascript/learn-regexp/LookAhead-LookBehind/look-ahead.js
+++ javascript/learn-regexp/LookAhead-LookBehind/look-ahead.js
@@ -9,6 +9,10 @@ let msg2 = targetString.match(/(e.)(?!\W)/g);
 // negative lookahead assertion: [ 'ell', 'end', 'er!' ]
 let msg3 = targetString.match(/(e.{2})(?!\W)/g);
 
+// positive lookahead assertion: 
+let msg4 = targetString.match(/(\s)(?=\d)/g);
+
 console.log(msg1);
 console.log(msg2);
 console.log(msg3);
+console.log(msg4);
