8bb4738895f4261732895d04ece06271abf2fd80
RinatValiullov <4rontender@gmail.com>
2020-01-17 14:53:53 +0500

    replaced asyncScheduler withy queueScheduler

--- javascript/learn-rxjs/use-scheduler/src/schedule.js
+++ javascript/learn-rxjs/use-scheduler/src/schedule.js
@@ -1,15 +1,16 @@
-import { Observable, asyncScheduler } from 'rxjs';
-import { observeOn } from 'rxjs/operators';
+import { Observable, asyncScheduler, queueScheduler } from 'rxjs';
+import { delay, observeOn } from 'rxjs/operators';
 
-const observable = new Observable(observer => {
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  observer.complete();
-}).pipe(observeOn(asyncScheduler));
+console.dir(queueScheduler);
 
-console.log('just before subscribe');
-observable.subscribe({
+const observable = new Observable(proxyObserver => {
+  proxyObserver.next(1);
+  proxyObserver.next(2);
+  proxyObserver.next(3);
+  proxyObserver.complete();
+}).pipe(observeOn(queueScheduler, 2000));
+
+const finalObserver = {
   next(x) {
     console.log('got value', x);
   },
@@ -19,6 +20,8 @@ observable.subscribe({
   complete() {
     console.log('done');
   }
-});
+};
 
+console.log('just before subscribe');
+observable.subscribe(finalObserver);
 console.log('just after subscribe');
