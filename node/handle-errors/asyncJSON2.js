a21855a53dad70e372eb84fde66daa89849b4448
RinatValiullov <4rontender@gmail.com>
2020-02-06 11:04:19 +0500

    added asyncJSON2 (with try..catch block)

---
+++ node/handle-errors/asyncJSON2.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const readJSON = (fileName, callback) => {
+  fs.readFile(fileName, 'utf8', function(error, result) {
+    if (error) {
+      return callback(error);
+    }
+    try {
+      callback.call(null, JSON.parse(result));
+    } catch (exept) {
+      callback(exept);
+    }
+  });
+};
+
+// Let's make an error artificially: typo in fileName
+const fileName = 'data.jso';
+
+const showArg = arg => console.log(arg);
+
+const resultJSON = readJSON(fileName, showArg);
+
+console.log(resultJSON);
+
+/*
+ *
+ * Conflates the input with the output ✕
+ *
+ * Doesn't work with control flow primitives ✕
+ *
+ * Handles errors in JSON.parse ✓
+ *
+ * Double handles errors in the callback ✕
+ *
+ */
--- node/handle-errors/syncJSON.js
+++ node/handle-errors/syncJSON.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const readJSONsync = fileName => JSON.parse(fs.readFileSync(fileName, 'utf8'));
 
+// Let's make an error artificially: typo in fileName
 const fileName = 'data.jso';
 
 const result = readJSONsync(fileName);
