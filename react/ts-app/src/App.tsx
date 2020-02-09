75095ad9ca91c6ec8a68f40a43c6ad4f6b1c7c8e
RinatValiullov <4rontender@gmail.com>
2020-02-09 21:14:10 +0500

    added react app (with typescript)

---
+++ react/ts-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>My first react app with typescript!</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
