e7564853c48f6911935ea6732dd15cf4e4a17bab
RinatValiullov <4rontender@gmail.com>
2020-02-11 22:28:30 +0500

    added typescript compilation

---
+++ typescript/sum-function/dist/app.js
@@ -0,0 +1,15 @@
+const inp1 = document.querySelector('#num1');
+const inp2 = document.querySelector('#num2');
+const addButton = document.querySelector('button');
+const add = (input1, input2) => {
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        return input1 + input2;
+    }
+    else {
+        return Number(input1) + Number(input2);
+    }
+};
+addButton.addEventListener('click', function () {
+    const resultSum = add(Number(inp1.value), Number(inp2.value));
+    console.log(resultSum);
+});
---
+++ typescript/sum-function/src/app.ts
@@ -0,0 +1,16 @@
+const inp1 = document.querySelector('#num1')! as HTMLInputElement;
+const inp2 = document.querySelector('#num2')! as HTMLInputElement;
+const addButton = document.querySelector('button');
+
+const add = (input1: number, input2: number): number => {
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    return input1 + input2;
+  } else {
+    return Number(input1) + Number(input2);
+  }
+};
+
+addButton.addEventListener('click', function () {
+  const resultSum: number = add(Number(inp1.value), Number(inp2.value));
+  console.log(resultSum);
+});
