1f9ab25ab4111e83e89ece392f243fb8e4351126
RinatValiullov <4rontender@gmail.com>
2020-02-13 21:34:19 +0500

    added "object-types" example

---
+++ typescript/basics/object-types/dist/app.js
@@ -0,0 +1,5 @@
+const person = {
+    name: 'Rinat Valiullov',
+    age: 34
+};
+console.log(person.name);
---
+++ typescript/basics/object-types/src/app.ts
@@ -0,0 +1,10 @@
+// const person: {
+//   name: string,
+//   age: number;
+// } = {
+const person = {
+  name: 'Rinat Valiullov',
+  age: 34
+};
+
+console.log(person.name);
