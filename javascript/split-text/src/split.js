c871ae425ce744fd0f0c4561836861f90233549e
RinatValiullov <4rontender@gmail.com>
2020-02-17 13:25:24 +0500

    added typescript

--- javascript/split-text/src/split.js
+++ javascript/split-text/src/split.js
@@ -1,16 +1,16 @@
-import { text } from './text';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const text_1 = require("./text");
 const segments = [];
 let startCounter = -1; // need think to change the logic but not the startCounter = 0
-
-for (let idx in text) {
-  if (text.charCodeAt(idx) === 44) {
-    // code '44' is for comma sign ','
-    segments.push(text.substring(startCounter + 1, idx));
-    startCounter = Number(idx) + 1;
-    console.log(`startCounter - ${startCounter} and idx - ${idx}`);
-  }
+for (let idx = 0; idx < text_1.text.length; idx++) {
+    let character = text_1.text.charCodeAt(idx);
+    if (character === 44) {
+        // code '44' is for comma sign ','
+        segments.push(text_1.text.substring(startCounter + 1, idx));
+        startCounter = Number(idx) + 1;
+        console.log(`startCounter - ${startCounter} and idx - ${idx}`);
+    }
 }
-segments.push(text.substring(startCounter + 1));
-
+segments.push(text_1.text.substring(startCounter + 1));
 console.log(segments);
---
+++ javascript/split-text/src/split.ts
@@ -0,0 +1,17 @@
+import { text } from './text';
+
+const segments = [];
+let startCounter = -1; // need think to change the logic but not the startCounter = 0
+
+for (let idx: number = 0; idx < text.length; idx++) {
+  let character = text.charCodeAt(idx);
+  if (character === 44) {
+    // code '44' is for comma sign ','
+    segments.push(text.substring(startCounter + 1, idx));
+    startCounter = Number(idx) + 1;
+    console.log(`startCounter - ${startCounter} and idx - ${idx}`);
+  }
+}
+segments.push(text.substring(startCounter + 1));
+
+console.log(segments);
