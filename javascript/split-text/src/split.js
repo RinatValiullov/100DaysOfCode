bd6c206d56ccd51fb051ab3dbe941b72aa54507d
RinatValiullov <4rontender@gmail.com>
2020-02-17 12:21:58 +0500

    changed babel config file and structure a little (need solve problem with unnecessary text.js compiling)

--- javascript/split-text/src/split.js
+++ javascript/split-text/src/split.js
@@ -1,5 +1,4 @@
-const text =
-  'Lorem ipsum dolor sit amet, ne aliquando consequuntur eum, vim et enim justo. Ne porro oblique est, nam ad justo sententiae elaboraret, utamur labores nonumes ei his. Ex illud abhorreant mei, eam tota noluisse vivendum cu. No vim ipsum integre concludaturque, eam ut soluta viderer. Ut qui utroque fierent, recusabo corrumpit pri te, lorem soluta his ne. Dicit doctus incorrupte id cum, ad sint molestie mea.';
+import { text } from './text';
 
 const segments = [];
 let startCounter = -1; // need think to change the logic but not the startCounter = 0
---
+++ javascript/split-text/src/text.js
@@ -0,0 +1,2 @@
+export const text =
+  'Lorem ipsum dolor sit amet, ne aliquando consequuntur eum, vim et enim justo. Ne porro oblique est, nam ad justo sententiae elaboraret, utamur labores nonumes ei his. Ex illud abhorreant mei, eam tota noluisse vivendum cu. No vim ipsum integre concludaturque, eam ut soluta viderer. Ut qui utroque fierent, recusabo corrumpit pri te, lorem soluta his ne. Dicit doctus incorrupte id cum, ad sint molestie mea.';
