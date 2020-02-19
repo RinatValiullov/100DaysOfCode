903cc633570bd21083bee3efc880d1444090b0a0
RinatValiullov <4rontender@gmail.com>
2020-02-19 22:27:26 +0500

    added literal type (resultConversion)

--- typescript/basics/any-union-type/src/app1.ts
+++ typescript/basics/any-union-type/src/app1.ts
@@ -1,15 +1,23 @@
-const combineAdd = (input1: number | string, input2: number | string) => {
+const combineAdd = (input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') => {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = Number(input1) + Number(input2);
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
+  // if (resultConversion === 'as-number') {
+  //   return Number(result);
+  // } else {
+  //   return result.toString();
+  // }
 };
 
-const combineNumbers = combineAdd(1, 5);
+const combineNumbers = combineAdd(1, 5, 'as-number');
 console.log(combineNumbers);
 
-const combineNames = combineAdd('Glacy ', 'Shonn');
+const combineStringRepresentations = combineAdd('1', '5', 'as-number');
+console.log(combineStringRepresentations);
+
+const combineNames = combineAdd('Glacy ', 'Shonn', 'as-text');
 console.log(combineNames);
