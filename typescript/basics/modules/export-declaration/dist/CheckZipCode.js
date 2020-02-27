8d35c29864018ffd4d163ed7715aaf97083e6638
RinatValiullov <4rontender@gmail.com>
2020-02-27 11:14:56 +0500

    fixed local gitignore file

--- typescript/basics/modules/export-declaration/dist/CheckZipCode.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ZipCodeValidator_1 = require("ZipCodeValidator");
-const show = new ZipCodeValidator_1.mainValidator();
-const result = show.isAcceptable('314000');
-console.log(result);
--- typescript/basics/modules/export-declaration/dist/ZipCodeValidator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.numberRegExp = /^[0-9]+$/;
-class ZipCodeValidator {
-    isAcceptable(zip) {
-        return zip.length >= 6 && exports.numberRegExp.test(zip);
-    }
-}
-exports.mainValidator = ZipCodeValidator;
