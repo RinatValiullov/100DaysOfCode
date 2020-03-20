5593cd53e7aa72916c12f790c4bdb1919bda9e72
RinatValiullov <4rontender@gmail.com>
2020-03-20 09:47:42 +0500

    added basic form validation

--- vue/employees-database/src/components/EmployeeForm.vue
+++ vue/employees-database/src/components/EmployeeForm.vue
@@ -2,9 +2,25 @@
   <div id="employee-form">
     <form @submit.prevent="handleSubmit">
       <label for="employeeName">Employee Name</label>
-      <input v-model="employee.name" type="text" id="employeeName" />
+      <input
+        type="text"
+        id="employeeName"
+        :class="{ 'has-error': submitting && invalidName }"
+        v-model="employee.name"
+        @focus="clearStatus"
+        @keypress="clearStatus"
+      />
+
       <label for="employeeEmail">Employee Email</label>
-      <input v-model="employee.email" type="text" id="employeeEmail" />
+      <input
+        type="text"
+        id="employeeEmail"
+        :class="{ 'has-error': submitting && invalidEmail }"
+        v-model="employee.email"
+        @focus="clearStatus"
+      />
+      <p v-if="error && submitting" class="error-message">❗ Please fill out all required fields</p>
+      <p v-if="success" class="success-message">✅ Employee successfully added</p>
       <button>Add Employee</button>
     </form>
   </div>
@@ -15,6 +31,9 @@ export default {
   name: "employee-form",
   data() {
     return {
+      submitting: false,
+      error: false,
+      success: false,
       employee: {
         name: "",
         email: ""
@@ -23,8 +42,34 @@ export default {
   },
   methods: {
     handleSubmit() {
-      // eslint-disable-next-line
+      this.submitting = true;
+      this.clearStatus();
+      if (this.invalidName || this.invalidEmail) {
+        this.error = true;
+        return;
+      }
+
       this.$emit("add:employee", this.employee);
+      this.employee = {
+        name: "",
+        email: ""
+      };
+      this.error = false;
+      this.success = true;
+      this.submitting = false;
+    },
+    clearStatus() {
+      this.success = false;
+      this.error = false;
+    }
+  },
+  computed: {
+    invalidName() {
+      return this.employee.name === "";
+    },
+
+    invalidEmail() {
+      return this.employee.email === "";
     }
   }
 };
@@ -33,5 +78,18 @@ export default {
 <style scoped>
 form {
   margin-bottom: 2px;
+}
+
+[class*="-message"] {
+  font-weight: 600;
+  font-style: italic;
+}
+
+.error-message {
+  color: rgba(231, 17, 24, 0.882);
+}
+
+.success-message {
+  color: rgba(80, 253, 0, 0.911);
 }
 </style>
