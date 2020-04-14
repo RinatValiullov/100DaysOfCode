0dba69ddd1224efefec8ba7ab3b655fdd23ac601
RinatValiullov <4rontender@gmail.com>
2020-04-14 16:27:20 +0500

    List Rendering

--- vue/product-page/main.js
+++ vue/product-page/main.js
@@ -4,6 +4,17 @@ const app = new Vue({
     product: 'Socks',
     image: './assets/image/vmSocks-green-onWhite.jpg',
     imageDescription: 'socks',
-    inStock: true
-  }
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 3245,
+        variantColor: 'green',
+      },
+      {
+        variantId: 1045,
+        variantColor: 'yellow',
+      },
+    ],
+  },
 });
