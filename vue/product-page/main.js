9927cb8998a52d820674f0a9471ddedc2b3893ca
RinatValiullov <4rontender@gmail.com>
2020-04-20 15:38:20 +0500

    added "info-tabs" component

--- vue/product-page/main.js
+++ vue/product-page/main.js
@@ -33,11 +33,8 @@ Vue.component('product', {
       <h1>{{ title }}</h1>
       <p v-if="inStock">In Stock</p>
       <p v-else :class="[ !inStock ? outOfStockStyle : '']">Out of Stock</p>
-      <p>Shipping: {{ shipping }}</p>
 
-      <product-details :details="details"></product-details>
-
-      <div class="showInfo">{{ sale }}</div>
+      <info-tabs :shipping="shipping" :details="details"></info-tabs>
 
       <div
         v-for="(variant, index) in variants"
@@ -263,6 +260,47 @@ Vue.component('product-tabs', {
     return {
       tabs: ['Reviews', 'Make a Review'],
       selectedTab: 'Reviews',
+    };
+  },
+});
+
+Vue.component('info-tabs', {
+  props: {
+    shipping: {
+      required: true,
+    },
+    details: {
+      type: Array,
+      required: true,
+    },
+  },
+  template: `
+    <div>
+      <ul>
+        <span class="tabs" 
+              :class="{ activeTab: selectedTab === tab }"
+              v-for="(tab, index) in tabs"
+              @click="selectedTab = tab"
+              :key="tab"
+        >{{ tab }}</span>
+      </ul>
+
+      <div v-show="selectedTab === 'Shipping'">
+        <p>{{ shipping }}</p>
+      </div>
+
+      <div v-show="selectedTab === 'Details'">
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+      </div>
+  
+    </div>
+  `,
+  data() {
+    return {
+      tabs: ['Shipping', 'Details'],
+      selectedTab: 'Shipping',
     };
   },
 });
