f966b537fa66ed582e2c35c90e1f188a7b81beab
RinatValiullov <4rontender@gmail.com>
2020-12-09 11:18:53 +0500

    add async pipeline example

--- javascript/optional-chaining-ava/src/pipeline-operator.test.js
+++ javascript/optional-chaining-ava/src/pipeline-operator.test.js
@@ -14,3 +14,20 @@ test('Simple pipeline usage', t => {
 
     t.is(result, 'Hello, hello!')
 });
+
+test('Partial application pipeline', t => {
+  let result = -5
+    |> (_ => Math.max(0, _));
+
+  t.is(result, 0);
+});
+
+test('Async pipeline', async t => {
+  const asyncAdd = number => Promise.resolve(number + 5);
+  const subtractOne = num1 => num1 - 1;
+  const result = 10
+    |> asyncAdd
+    |> (async (num) => subtractOne(await num));
+
+  t.is(await result, 14);
+});
