0d0157e8cc51f82231513dcb27380d4e09986d4e
Rinat Valiullov <4rontender@gmail.com>
2021-09-20 22:21:08 +0500

    Add the ability to select the active slide programmatically.

--- javascript/vertical-gallery/app.js
+++ javascript/vertical-gallery/app.js
@@ -1,16 +1,28 @@
-const slides = document.querySelectorAll(".slide");
+const slidesPlugin = (activeSlide = 0) => {
+  const slides = document.querySelectorAll(".slide");
 
-const clearActiveClass = () => {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
+  if (activeSlide >= 0 && activeSlide <= 4) {
+    slides[activeSlide].classList.add("active");
+  } else {
+    console.error(
+      `You must pass a number in range [0..4] to the function. ${activeSlide} is not in this range!`
+    );
+  }
+
+  const clearActiveClass = () => {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  };
+
+  for (const slide of slides) {
+    slide.addEventListener("click", (ev) => {
+      clearActiveClass();
+      if (!slide.classList.contains("active")) {
+        slide.classList.add("active");
+      }
+    });
+  }
 };
 
-for (const slide of slides) {
-  slide.addEventListener("click", (ev) => {
-    clearActiveClass();
-    if (!slide.classList.contains("active")) {
-      slide.classList.add("active");
-    }
-  });
-}
+slidesPlugin(3);
