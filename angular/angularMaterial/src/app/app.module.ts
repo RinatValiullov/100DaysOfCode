9a1376fe55ee40e175581f706cad52d5a87d45f5
Rinat Valiullov <4rontender@gmail.com>
2021-10-11 23:27:21 +0500

    add button with icon (just to check)

--- angular/angularMaterial/src/app/app.module.ts
+++ angular/angularMaterial/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
