75dee92bc9372e96fcea81b65e3e863553e91410
Rinat Valiullov <4rontender@gmail.com>
2021-10-13 01:15:40 +0500

    add "contactmanager" component and "contactmanager.module"

--- angular/angularMaterial/src/app/app.module.ts
+++ angular/angularMaterial/src/app/app.module.ts
@@ -7,8 +7,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  { path: 'contactmanager', loadChildren: () => import('./contactmanager/contactmanager.module').then(m => m.ContactmanagerModule) },
   { path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule) },
-  { path: '**', redirectTo: 'demo' }
+  { path: '**', redirectTo: 'contactmanager' }
 ];
 
 @NgModule({
