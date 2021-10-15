d98713612afc982d682573693bccb1bf4883ba78
Rinat Valiullov <4rontender@gmail.com>
2021-10-15 15:58:08 +0500

    add "sidenav-toggle" button

--- angular/angularMaterial/src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ angular/angularMaterial/src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
+  @Output() toggleSidenav = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit(): void {
