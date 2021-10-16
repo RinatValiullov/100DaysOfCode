6c253c1bcca006bf79dd7914d333ae534433631b
Rinat Valiullov <4rontender@gmail.com>
2021-10-17 01:36:58 +0500

    add svg avatars

--- angular/angularMaterial/src/app/app.module.ts
+++ angular/angularMaterial/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,6 +20,7 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
