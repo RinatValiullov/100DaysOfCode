c6a072096b3241acf1d667656b106dcc88354140
Rinat Valiullov <4rontender@gmail.com>
2021-10-18 03:34:46 +0500

    add sorting

--- angular/angularMaterial/src/app/contactmanager/components/notes/notes.component.ts
+++ angular/angularMaterial/src/app/contactmanager/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Note } from '../../models/note';
 
@@ -18,13 +19,15 @@ export class NotesComponent implements OnInit, AfterViewInit {
   constructor() { }
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource<Note>(this.notes)
+    this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
 
   applyFilter(event: Event) {
