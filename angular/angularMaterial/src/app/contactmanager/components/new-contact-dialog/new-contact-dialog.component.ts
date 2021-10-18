6177e42992140c9463f7b17600a72793e3c14f13
Rinat Valiullov <4rontender@gmail.com>
2021-10-18 17:54:36 +0500

    set the form in dialog "New Contact" component

--- angular/angularMaterial/src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ angular/angularMaterial/src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -12,4 +12,9 @@ export class NewContactDialogComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  save() { }
+
+
+  dismiss() { }
+
 }
--- angular/angularMaterial/src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ angular/angularMaterial/src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -16,8 +16,12 @@ export class ToolbarComponent implements OnInit {
   }
 
   openAddContactDialog(): void {
-    this.dialog.open(NewContactDialogComponent, {
+    let dialogRef = this.dialog.open(NewContactDialogComponent, {
       width: '450px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed', result);
     });
   }
 
