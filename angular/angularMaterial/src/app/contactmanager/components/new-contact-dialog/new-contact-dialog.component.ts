f58594972a5f6fa51f69f79db13f2546d613faf3
Rinat Valiullov <4rontender@gmail.com>
2021-10-18 18:20:11 +0500

    add form validation

--- angular/angularMaterial/src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ angular/angularMaterial/src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { User } from '../../models/user';
 
@@ -19,11 +20,18 @@ export class NewContactDialogComponent implements OnInit {
 
   constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
 
+  name = new FormControl('', [Validators.required]);
+
+  getErrorMessage() {
+    return this.name.hasError('required') ? 'You must enter a name' : '';
+  }
+
   ngOnInit(): void {
     this.user = new User();
   }
 
   save() {
+    this.user.name = this.name.value;
     this.dialogRef.close(this.user);
   }
 
