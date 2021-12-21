eb57da4c05ec84c6cc9f8e94d7ba6ad59b2b3f42
Rinat Valiullov <4rontender@gmail.com>
2021-12-22 03:19:30 +0500

    add "publicPath" option to webpack config

--- webpack/start/webpack.config.js
+++ webpack/start/webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "./dist")
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "dist/"
   },
   module: {
     rules: [
