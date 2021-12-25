057cd103e2720f9740d428686b09409ba0063291
Rinat Valiullov <4rontender@gmail.com>
2021-12-25 23:29:10 +0500

    clean folders before generating bundles with "clean-webpack-plugin"

--- webpack/start/webpack.config.js
+++ webpack/start/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "none",
@@ -50,6 +51,12 @@ module.exports = {
     new TerserPlugin(),
     new MiniCssExtractPlugin({
       filename: "styles.[contenthash].css"
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        "**/*",
+        path.join(process.cwd(), "build/**/*")
+      ]
     })
   ]
 };
