06286313fe945d8f3dc92d59029c1eee2f983c1e
Rinat Valiullov <4rontender@gmail.com>
2021-12-28 04:54:16 +0500

    serve static html files via "Express"

--- webpack/start/src/server.js
+++ webpack/start/src/server.js
@@ -1,10 +1,16 @@
+const path = require("path");
+const fs = require("fs");
+
 const express = require("express");
 const app = express();
 
 const PORT = 4327;
 
 app.get("/", (req, res) => {
-  res.send("Server on express framework");
+  const pathToIndexFile = path.resolve(__dirname, "../dist/index.html");
+  const contentFromIndexFile = fs.readFileSync(pathToIndexFile, "utf-8");
+  res.send(contentFromIndexFile);
+  console.log(pathToIndexFile);
 });
 
 app.listen(PORT, () => {
