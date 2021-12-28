179f34b8b84b2d1c535569e8a6321b4c30a9c37a
Rinat Valiullov <4rontender@gmail.com>
2021-12-28 05:50:22 +0500

    code splitting (multiple CSS and JS bundles)

--- webpack/start/src/adrian.js
+++ webpack/start/src/adrian.js
@@ -1,4 +1,8 @@
+import { Heading } from "./components/heading/heading";
 import { AdrianImage } from "./components/AdrianImage/AdrianImage";
+
+const heading = new Heading();
+heading.render();
 
 const adrianImage = new AdrianImage();
 adrianImage.render();
--- webpack/start/webpack.production.config.js
+++ webpack/start/webpack.production.config.js
@@ -5,9 +5,12 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  entry: "./src/index.js",
+  entry: {
+    hello: "./src/hello.js",
+    adrian: "./src/adrian.js"
+  },
   output: {
-    filename: "bundle.[contenthash].js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
     publicPath: "/static/"
   },
@@ -53,7 +56,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "styles.[contenthash].css"
+      filename: "[name].[contenthash].css"
     }),
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [
