be7e6aa584bcb9ab70068b95c348b3e8dfdc12be
Rinat Valiullov <4rontender@gmail.com>
2021-12-30 01:35:55 +0500

    setting up Module Federation

--- webpack/separate-apps/hello-world/webpack.dev.config.js
+++ webpack/separate-apps/hello-world/webpack.dev.config.js
@@ -1,13 +1,14 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
 
 module.exports = {
   entry: "./src/hello-world.js",
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: ""
+    publicPath: "http://localhost:9001/"
   },
   mode: "development",
   devServer: {
@@ -45,6 +46,14 @@ module.exports = {
       title: "Hello world",
       description: "Hello world",
       template: "src/page-template.hbs"
+    }),
+    new ModuleFederationPlugin({
+      name: "HelloWorldApp",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./HelloWorldButton":
+          "./src/components/hello-world-button/hello-world-button.js"
+      }
     })
   ]
 };
