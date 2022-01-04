c8d9a281466b27587aad4c160a33279fb8d86246
Rinat Valiullov <4rontender@gmail.com>
2022-01-04 23:13:41 +0500

    integrate "jQuery" lib to the app

--- webpack/separate-apps/hello-world/src/components/heading/heading.js
+++ webpack/separate-apps/hello-world/src/components/heading/heading.js
@@ -1,12 +1,13 @@
-import './heading.scss';
+import "./heading.scss";
+import $ from "jquery";
 
 class Heading {
-    render(pageName) {
-        const h1 = document.createElement('h1');
-        const body = document.querySelector('body');
-        h1.innerHTML = 'Webpack is awesome. This is "' + pageName + '"page';
-        body.appendChild(h1);
-    }
+  render(pageName) {
+    const h1 = $("<h1>");
+    const body = $("body");
+    h1.text('Webpack is awesome. This is "' + pageName + '"page');
+    body.append(h1);
+  }
 }
 
 export default Heading;
