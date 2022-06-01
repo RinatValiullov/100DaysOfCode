f577d3fa8b74238c721bf15dab4c4c38fdaba969
Rinat Valiullov <4rontender@gmail.com>
2022-06-01 15:10:35 +0500

    add "styles" gulp task

--- gulp/compress-images/gulpfile.js
+++ gulp/compress-images/gulpfile.js
@@ -1,3 +1,40 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, series, parallel, watch } = require("gulp");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const cleancss = require("gulp-clean-css");
+const autoPrefixer = require("gulp-autoprefixer");
 
-exports.default = def;
+function bs(cb) {
+  browserSync.init({
+    server: {
+      baseDir: "./src"
+    },
+    notify: false,
+    online: true
+  });
+}
+
+function styles(cb) {
+  return src(["./src/styles/main.css", "./src/styles/reset.css"])
+    .pipe(concat("app.min.css"))
+    .pipe(
+      autoPrefixer({
+        overrideBrowserlist: ["last 5 versions"],
+        grid: true
+      })
+    )
+    .pipe(
+      cleancss({
+        level: { 1: { specialComments: 0 } } /*, format: 'beautify' */
+      })
+    )
+    .pipe(dest("./src/dist/css/"))
+    .pipe(browserSync.stream());
+}
+
+function startWatch() {
+  watch(["./src/styles/**/*"], styles);
+}
+
+exports.styles = styles;
+exports.default = parallel(styles, bs, startWatch);
