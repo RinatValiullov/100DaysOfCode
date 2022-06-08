b61f2a82f8fb7f47cdf8ef0d9c5b33c477c44f09
Rinat Valiullov <4rontender@gmail.com>
2022-06-08 23:24:45 +0500

    parse input data

--- node/weatherapp-express/server.js
+++ node/weatherapp-express/server.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const PORT = process.env.PORT || 4327;
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index", { nickname: "4rontender" });
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  const { city } = req.body;
+
+  console.log(city);
+
+  res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server is runing on ${PORT} port...`));
