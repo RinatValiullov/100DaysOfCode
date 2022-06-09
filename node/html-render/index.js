d0fbcccaf290629db55d6309cebfd51ffc82b36d
Rinat Valiullov <4rontender@gmail.com>
2022-06-09 23:24:31 +0500

    fix "express-handlebars" version issue

--- node/html-render/index.js
+++ node/html-render/index.js
@@ -1,20 +1,28 @@
-let path = require('path');
-let express = require('express');
-let exphbs = require('express-handlebars');
+let path = require("path");
+let express = require("express");
+let exphbs = require("express-handlebars");
+
 let app = express();
 
-app.engine('.hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  layoutsDir: path.join(__dirname, 'views/layouts')
-}));
+app.engine(
+  ".hbs",
+  exphbs.engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "views/layouts")
+  })
+);
 
-app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'views'));
-app.listen(4327);
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (request, response) => {
-  response.render('home', {
-    name: '4rontender'
+app.get("/", (request, response) => {
+  response.render("home", {
+    name: "4rontender",
+    languages: ["JavaScript", "PHP", "Python"]
   });
 });
+
+const PORT = process.env.PORT || 4327;
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT} port...`));
