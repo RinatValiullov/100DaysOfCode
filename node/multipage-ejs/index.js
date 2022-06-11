383313bdf86de1708fe09c65cc87248fa0f73635
Rinat Valiullov <4rontender@gmail.com>
2022-06-11 22:59:00 +0500

    post data from the form

--- node/multipage-ejs/index.js
+++ node/multipage-ejs/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 4327;
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Index page
 app.get("/", (req, res) => {
@@ -13,6 +15,14 @@ app.get("/", (req, res) => {
 // about page
 app.get("/about", (req, res) => {
   res.render("pages/about");
+});
+
+app.post("/", (req, res) => {
+  const { city } = req.body;
+
+  console.log(city);
+
+  res.render("pages/index");
 });
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT} port...`));
