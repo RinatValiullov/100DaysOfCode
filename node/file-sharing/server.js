24958a577a0b6de3eb9758b564672c6a6186d63e
Rinat Valiullov <4rontender@gmail.com>
2022-07-01 06:02:37 +0500

    file upload server logic

--- node/file-sharing/server.js
+++ node/file-sharing/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const File = require("./models/File");
 
 const app = express();
 
@@ -17,8 +19,18 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.post("/upload", upload.single("file"), (req, res) => {
-  res.send("hi");
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname
+  };
+  if (req.body.password !== null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  const file = await File.create(fileData);
+  console.log(file);
+  res.send(file.originalName);
 });
 
 app.listen(PORT, console.log(`Server is running on ${PORT} port...`));
