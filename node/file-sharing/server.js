9dcb72ce44cbfeef1e84c051c3d8881cb6ef9c9d
Rinat Valiullov <4rontender@gmail.com>
2022-07-02 16:40:35 +0500

    password protection logic

--- node/file-sharing/server.js
+++ node/file-sharing/server.js
@@ -6,6 +6,8 @@ const bcrypt = require("bcrypt");
 const File = require("./models/File");
 
 const app = express();
+app.use(express.static(__dirname + "/styles"));
+app.use(express.urlencoded({ extended: true }));
 
 const upload = multer({ dest: "uploads" });
 
@@ -33,14 +35,34 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
 });
 
-app.get("/file/:id", async (req, res) => {
+// app.get("/file/:id", handleDownload);
+// app.post("/file/:id", handleDownload);
+
+// The same as above lines: 38, 39
+app.route("/file/:id").get(handleDownload).post(handleDownload);
+
+async function handleDownload(req, res) {
   const file = await File.findById(req.params.id);
 
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password");
+      return;
+    }
+
+    // Be careful with strict and loose comparing: lines 47 and 48.
+    // "req.body.password" is just a string. But "file.password" is a hash string.
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render("password", { error: true });
+      return;
+    }
+  }
+
   file.downloadCount++;
-  file.save();
-  console.log(file.downloadCount);
+  await file.save();
+  console.log(file.path);
 
   res.download(file.path, file.originalName);
-});
+}
 
 app.listen(PORT, console.log(`Server is running on ${PORT} port...`));
