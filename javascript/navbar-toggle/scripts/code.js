427c7ba45818eadf3eb8a92e94f962e6b11dd165
Rinat Valiullov <4rontender@gmail.com>
2022-07-11 23:00:57 +0500

    add toggle logic for social icons too

--- javascript/navbar-toggle/scripts/code.js
+++ javascript/navbar-toggle/scripts/code.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+const social = document.querySelector(".social-icons");
 
 navToggle.addEventListener("click", () => {
   links.classList.toggle("show-links");
+  social.classList.toggle("show-social");
 });
